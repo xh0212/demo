7e369ce6d771b1840bfffa336bc9747d58881925
小花 <18010269090@163.com>
2019-01-24 15:31:43 +0800

    1.24

--- elm2/src/router/router.js
+++ elm2/src/router/router.js
@@ -1,6 +1,8 @@
 import App from '../App'
 
 const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
+const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
 
 
 
@@ -20,6 +22,15 @@ export default [{
             path: '/home',
             component: home
         },
-        
+        //当前选择城市页
+        {
+            path: '/city/:cityid',
+            component: city
+        },
+        //登录注册页
+        {
+            path: '/login',
+            component: login
+        },
     ]
 }]
