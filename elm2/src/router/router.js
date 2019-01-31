d489cb35e8b2160f87018da735d7a1fcc4736987
小花 <18010269090@163.com>
2019-01-31 21:14:23 +0800

    1月31日

--- elm2/src/router/router.js
+++ elm2/src/router/router.js
@@ -16,6 +16,20 @@ const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'sho
 const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
 const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
 const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
+const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
+const balance = r => require.ensure([], () => r(require('../page/balance/balance')), 'balance')
+const balanceDetail = r => require.ensure([], () => r(require('../page/balance/children/detail')), 'balanceDetail')
+const benefit = r => require.ensure([], () => r(require('../page/benefit/benefit')), 'benefit')
+const coupon = r => require.ensure([], () => r(require('../page/benefit/children/coupon')), 'coupon')
+const hbDescription = r => require.ensure([], () => r(require('../page/benefit/children/hbDescription')), 'hbDescription')
+const hbHistory = r => require.ensure([], () => r(require('../page/benefit/children/hbHistory')), 'hbHistory')
+const exchange = r => require.ensure([], () => r(require('../page/benefit/children/exchange')), 'exchange')
+const commend = r => require.ensure([], () => r(require('../page/benefit/children/commend')), 'commend')
+const points = r => require.ensure([], () => r(require('../page/points/points')), 'points')
+const pointsDetail = r => require.ensure([], () => r(require('../page/points/children/detail')), 'pointsDetail')
+const service = r => require.ensure([], () => r(require('../page/service/service')), 'service')
+const questionDetail = r => require.ensure([], () => r(require('../page/service/children/questionDetail')), 'questionDetail')
+const download = r => require.ensure([], () => r(require('../page/download/download')), 'download')
 
 
 
@@ -81,7 +95,11 @@ export default [{
                         }]
                     }]
                 }]
-            }]
+            },
+            {
+                path: 'service', //服务中心
+                component: service,
+            },]
         },
          //商铺详情页
          {
@@ -98,6 +116,64 @@ export default [{
                     component: shopSafe,
                 }, ]
             }]
+        },
+        //订单列表页
+        {
+            path: '/order',
+            component: order,
+            
+        },
+        //余额
+        {
+            path: 'balance',
+            component: balance,
+            children: [{
+                path: 'detail', //余额说明
+                component: balanceDetail,
+            }, ]
+        },
+         //我的优惠页
+         {
+            path: 'benefit',
+            component: benefit,
+            children: [{
+                path: 'coupon', //代金券说明
+                component: coupon,
+            },{
+                path: 'hbDescription', //红包说明
+                component: hbDescription,
+            },{
+                path: 'hbHistory', //历史红包
+                component: hbHistory,
+            },{
+                path: 'exchange', //兑换红包
+                component: exchange,
+            },{
+                path: 'commend', //推荐有奖
+                component: commend,
+            }]
+        },
+        {
+            path: 'points',
+            component: points,
+            children: [{
+                path: 'detail', //积分说明
+                component: pointsDetail,
+            }, ]
+        },
+        //服务中心
+        {
+            path: '/service',
+            component: service,
+            children: [{
+                path: 'questionDetail', //订单详情页
+                component: questionDetail,
+            }, ]
+        },
+         //下载页
+         {
+            path: '/download',
+            component: download
         },
     ]
 }]
